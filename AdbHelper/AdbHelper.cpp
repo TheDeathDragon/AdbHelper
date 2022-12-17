ef350cb681aa2c9a2be750a9c95146338a7b4ff6
TheDeathDragon <249501281@qq.com>
2022-12-17 17:43:00 +0800

    添加一些选项

--- AdbHelper/AdbHelper.cpp
+++ AdbHelper/AdbHelper.cpp
@@ -25,6 +25,7 @@ int main()
 	string apkExt(".apk");
 	string apkCurrent("");
 	string tempString("");
+	string powershell("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe");
 
 	int choice = 0;
 	int cameraShotCount = 0;
@@ -33,18 +34,23 @@ int main()
 
 	while (true) {
 		cout << "\n便捷ADB小工具菜单：\n" << endl;
-		cout << "0 > 退出程序" << endl;
-		cout << "1 > 导出DebugLog(/data/debuglogger/)" << endl;
-		cout << "2 > 导出DebugLog(/sdcard/debuglogger/)" << endl;
-		cout << "3 > 导出相片、视频(/sdcard/DCIM/)" << endl;
-		cout << "4 > 导出屏幕录像(/sdcard/Movies/)" << endl;
-		cout << "5 > 导出屏幕截图(/sdcard/Pictures/)" << endl;
-		cout << "6 > 查看当前Activity" << endl;
-		cout << "7 > 一键Push文件到Sdcard目录，文件需放在Files中" << endl;
-		cout << "8 > 一键批量安装APK，文件需放在Apks中" << endl;
-		cout << "9 > 抓取ADBLog" << endl;
+		cout << "0  > 退出程序" << endl;
+		cout << "1  > 导出DebugLog(/data/debuglogger/)" << endl;
+		cout << "2  > 导出DebugLog(/sdcard/debuglogger/)" << endl;
+		cout << "3  > 导出相片、视频(/sdcard/DCIM/)" << endl;
+		cout << "4  > 导出屏幕录像(/sdcard/Movies/)" << endl;
+		cout << "5  > 导出屏幕截图(/sdcard/Pictures/)" << endl;
+		cout << "6  > 查看当前Activity" << endl;
+		cout << "7  > 一键Push文件到Sdcard目录，文件需放在Files中" << endl;
+		cout << "8  > 一键批量安装APK，文件需放在Apks中" << endl;
+		cout << "9  > 抓取ADBLog(保存为system.log)" << endl;
 		cout << "10 > 抓取屏幕截图" << endl;
 		cout << "11 > 循环拍照测试" << endl;
+		cout << "12 > 导出电池曲线(/sdcard/BatteryLog.csv)" << endl;
+		cout << "13 > 查看手机上安装的第三方软件" << endl;
+		cout << "14 > 查看设备FingerPrint" << endl;
+		cout << "15 > 查看设备屏幕分辨率和DPI" << endl;
+		cout << "16 > 抓取Kernel Log(保存为dmesg.log)" << endl;
 
 		cout << "\n请选择 > ";
 		cin >> choice;
@@ -99,8 +105,10 @@ int main()
 			}
 			break;
 		case 9:
-			cout << "日志文件保存为temp.log，按Ctrl+C结束" << endl;
-			system("adb logcat -b all > temp.log");
+			cout << "日志文件保存为system.log，按Ctrl+C结束" << endl;
+			tempString = powershell + " \"adb logcat -b all 2>&1 | tee system.log\"";
+			system(tempString.c_str());
+			tempString = "";
 			break;
 		case 10:
 			cout << "抓取的截图保存在当前文件夹" << endl;
@@ -124,8 +132,31 @@ int main()
 				Sleep(1 * 1000);
 			}
 			break;
+		case 12:
+			system("adb pull /sdcard/BatteryLog.csv");
+			break;
+		case 13:
+			system("adb shell pm list packages -3");
+			break;
+		case 14:
+			system("adb shell \"getprop | grep ro.vendor.build.fingerprint\"");
+			system("adb shell \"getprop | grep product.product\"");
+			break;
+		case 15:
+			system("adb shell wm size");
+			system("adb shell wm density");
+			break;
+		case 16:
+			cout << "日志文件保存为dmesg.log，按Ctrl+C结束" << endl;
+			tempString = powershell + " \"adb shell 'dmesg -w' 2>&1 | tee dmesg.log\"";
+			system(tempString.c_str());
+			tempString = "";
+			break;
 		default:
 			break;
 		}
+		cout << "\n按回车继续..." << endl;
+		getchar();
+		getchar();
 	}
 }
