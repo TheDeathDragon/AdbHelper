273c8683b10664eccd79045afea5e275865c377d
那年雪落 <249501281@qq.com>
2023-06-03 10:47:48 +0800

    添加导出SD卡中的YLog 导出内部存储中的MtkLog

--- AdbHelper/AdbHelper.cpp
+++ AdbHelper/AdbHelper.cpp
@@ -1,9 +1,12 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <filesystem>
-#include<vector>
-#include<string>
+#include <vector>
+#include <string>
 #include <ctime>
+#include <memory>
+#include <stdexcept>
+#include <array>
 using namespace std;
 namespace fs = std::filesystem;
 
@@ -20,12 +23,49 @@ bool dirExists(const std::string& path) {
 	return false;
 }
 
+std::string exec(const char* cmd) {
+	std::array<char, 128> buffer;
+	std::string result;
+	std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
+	if (!pipe) {
+		throw std::runtime_error("_popen() failed!");
+	}
+	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+		result += buffer.data();
+	}
+	return result;
+}
+
+bool checkAdbConnect() {
+
+	string tempString("");
+	string cmdOutput("");
+	tempString = "adb get-state";
+	cmdOutput = exec(tempString.c_str());
+	if (!cmdOutput.empty()) {
+		cmdOutput.erase(cmdOutput.size() - 1);
+		if (cmdOutput == "device") {
+			return true;
+		}
+	}
+	else {
+		cout << "\n没找到连接的ADB设备，或者有多台ADB设备，请确保当前只有一台设备再操作！！\n" << endl;
+		cout << "请打开设置，连续点击[关于手机]-[版本号]5次打开开发者模式，\n" << endl;
+		cout << "然后[返回]-[系统]-[开发者选项]，进入开发者选项设置界面，\n" << endl;
+		cout << "找到[调试]-[USB调试]，打开开关即可！\n" << endl;
+		system("pause");
+		system("cls");
+	}
+	return false;
+}
+
 int main()
 {
 	string apkPath("Apks");
 	string apkExt(".apk");
 	string apkCurrent("");
 	string tempString("");
+	string cmdOutput("");
 	string powershell("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe");
 
 	int choice = 0;
@@ -47,9 +87,11 @@ int main()
 
 	std::cout << "当前系统时间: " << formatted_time << std::endl;
 
-	cout << "\n请先确保ADB能正常工作！(当前版本：" << __DATE__ << ")" << endl;
+	cout << "\n请先确保ADB能正常工作！(当前版本：" << __DATE__ << ")\n" << endl;
 
 	while (true) {
+	checkAdb:
+		if (!checkAdbConnect()) goto checkAdb;
 		cout << "\n便捷ADB小工具菜单：\n" << endl;
 		cout << "0  > 退出程序" << endl;
 		cout << "1  > 导出MTK DebugLog(/data/debuglogger/)" << endl;
@@ -71,6 +113,8 @@ int main()
 		cout << "17 > 结束Monkey测试" << endl;
 		cout << "18 > 亮灭屏测试" << endl;
 		cout << "19 > 触发展讯SystemDump" << endl;
+		cout << "20 > 导出SD卡中的YLog(/storage/SD卡ID/ylog)" << endl;
+		cout << "21 > 导出内部存储中的MtkLog(adb pull /sdcard/debuglogger)" << endl;
 
 		cout << "\n请选择 > ";
 		cin >> choice;
@@ -107,18 +151,16 @@ int main()
 			if (!dirExists("Files")) {
 				system("mkdir Files");
 			}
-			cout << "请把要Push的文件都放到Files文件夹里面，按回车开始Push！" << endl;
-			getchar();
-			getchar();
+			cout << "请把要Push的文件都放到Files文件夹里面，按任意键开始Push！\n" << endl;
+			system("pause");
 			system("cd .\\Files\\ & adb push . /sdcard/");
 			break;
 		case 8:
 			if (!dirExists("Apks")) {
 				system("mkdir Apks");
 			}
-			cout << "请把安装包文件都放到Apks文件夹里面，按回车开始安装！" << endl;
-			getchar();
-			getchar();
+			cout << "请把安装包文件都放到Apks文件夹里面！\n" << endl;
+			system("pause");
 			for (auto& p : fs::recursive_directory_iterator(apkPath))
 			{
 				if (p.path().extension() == apkExt) {
@@ -199,19 +241,37 @@ int main()
 			}
 			break;
 		case 19:
-			cout << "请确保在进行SystemDump之前，插入了SD卡并且可用空间大于3G！" << endl;
-			cout << "\n按回车继续！\n" << endl;
+			cout << "请确保在进行SystemDump之前，插入了SD卡并且可用空间大于3G！\n" << endl;
 			system("adb root");
-			getchar();
-			getchar();
+			system("pause");
 			system("adb shell \"echo 'c' > /proc/sysrq-trigger\"");
 			cout << "\n已经下发指令给设备，设备将会重启进入SystemDump模式！" << endl;
+		case 20:
+			tempString = "adb shell \"sm list-volumes public | cut -d' ' -f3\"";
+			cmdOutput = exec(tempString.c_str());
+			if (!cmdOutput.empty()) {
+				cmdOutput.erase(cmdOutput.size() - 1);
+				cout << "SD卡的ID：\n" << cmdOutput << endl;
+				tempString = "adb pull /storage/" + cmdOutput + "/ylog";
+				cout << "[" << tempString << "]" << endl;
+				system(tempString.c_str());
+			}
+			else {
+				cout << "没找到SD卡，请查看SD卡是否正确插入？" << endl;
+				continue;
+			}
+			break;
+		case 21:
+			tempString = "adb pull /sdcard/debuglogger/ debuglogger_";
+			tempString += formatted_time;
+			system(tempString.c_str());
+			tempString = "";
+			break;
 		default:
 			break;
 		}
-		cout << "\n按回车继续..." << endl;
-		getchar();
-		getchar();
+		cout << endl;
+		system("pause");
 		system("cls");
 	}
 }
